5de34d2aac837de3e0ef17dbbbadc89523811f0d
Karolina <karolina.maison1@gmail.com>
2019-08-15 13:40:04 +0200

    Create project

---
+++ Client/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+using System.Text;
+
+//http://127.0.0.1/ViewGetter/GetView/index.html
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceHost = new ServiceHost(typeof(ViewGetter));
+            var serviceEndpoint = serviceHost.AddServiceEndpoint(typeof(IViewGetter), new WebHttpBinding(), "http://127.0.0.1/ViewGetter");
+            serviceEndpoint.Behaviors.Add(new WebHttpBehavior());
+            serviceHost.Open();
+
+            var apiServiceHost = new ServiceHost(typeof(Api));
+            var apiServiceEndpoint = apiServiceHost.AddServiceEndpoint(typeof(IApi), new WebHttpBinding(), "http://127.0.0.1/api");
+            apiServiceEndpoint.Behaviors.Add(new WebHttpBehavior());
+            apiServiceHost.Open();
+
+            Console.ReadLine();
+            serviceHost.Close();
+            apiServiceHost.Close();
+        }
+    }
+
+    [ServiceContract]
+    public interface IViewGetter
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "GetView/{url}")]
+        Stream GetView(string url);
+    }
+
+    public class ViewGetter : IViewGetter
+    {
+        public Stream GetView(string url)
+        {
+            using (var stream = new StreamReader($"./App_Data/{url}"))
+            {
+                var data = stream.ReadToEnd();
+                byte[] resultBytes = Encoding.UTF8.GetBytes(data);
+                WebOperationContext.Current.OutgoingResponse.ContentType = "text/html";
+
+                return new MemoryStream(resultBytes);
+            }
+        }
+    }
+    [ServiceContract]
+    public interface IApi
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "AddProduct", BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            RequestFormat = WebMessageFormat.Json)]
+        bool AddProduct(Product product);
+        void Index();
+        [WebGet]
+        List<Product> ViewProducts();
+    }
+
+    public class Api : IApi
+    {
+        public bool AddProduct(Product product)
+        {
+            var fact = new ChannelFactory<IService1>(new BasicHttpBinding(),
+                new EndpointAddress("http://localhost:28732/Service1.svc?singleWsdl"));
+            var client = fact.CreateChannel();
+
+            return client.AddProduct(product.Key, product.Size, product.Color, product.Price, product.Type, product.Amount);
+        }
+        public void Index()
+        {
+            var fact = new ChannelFactory<IService1>(new BasicHttpBinding(),
+               new EndpointAddress("http://localhost:28732/Service1.svc?singleWsdl"));
+            var client = fact.CreateChannel();
+        }
+
+        public List<Product> ViewProducts()
+        {
+            ChannelFactory<IService1> fact = new ChannelFactory<IService1>(new BasicHttpBinding(),
+                new EndpointAddress("http://localhost:28732/Service1.svc?singleWsdl"));
+            var client = fact.CreateChannel();
+            var products = client.GetProducts(); //return null :/
+
+            return client.GetProducts(); 
+        }
+    }
+
+}
---
+++ Client/bin/Debug/App_Data/scripts.js
@@ -0,0 +1,74 @@
+function DoAjax(method, url, fn, params) {
+	var request = new XMLHttpRequest();
+	request.open(method, url, true);
+	request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+	var handler = function (request) {
+		return function () {
+			if (request.readyState != 4) return;
+			if (request.status == 200) fn(request);
+			else alert(request.readyState + " " + request.status + " " + request.statusText);
+		};
+	}
+	request.onreadystatechange = handler(request);
+	request.send(JSON.stringify(params));
+}
+
+function AddProduct() {
+	var fn = function (request) {
+		var e = document.getElementById("wynik");
+		e.innerHTML = request.responseXML.childNodes[0].childNodes[0].nodeValue;
+		e = document.getElementById("resp");
+		e.innerHTML = request.responseText;
+	};
+	var key, size, color, price, type, amount;
+	key = document.getElementById("key").value;
+	size = document.getElementById("size").value;
+	color = document.getElementById("color").value;
+	price = document.getElementById("price").value;
+	type = document.getElementById("type").value;
+	amount = document.getElementById("amount").value;
+
+	var params = {
+		"product": {
+			"Key": key,
+			"Size": size,
+			"Color": color,
+			"Price": price,
+			"Type": type,
+			"Amount": amount
+		}
+
+	};
+
+	DoAjax("POST", "http://127.0.0.1/api/AddProduct", fn, params);
+}
+
+function ViewProducts() {
+	var fn = function (request) {
+		var e = document.getElementById("wynik");
+		e.innerHTML = request.responseXML.childNodes[0].childNodes[0].nodeValue;
+		e = document.getElementById("resp");
+		e.innerHTML = request.responseText;
+	};
+	var key, size, color, price, type, amount;
+	key = 0;
+	size = 0;
+	color = 0;
+	price = 0;
+	type = 0;
+	amount = 0;
+
+	var params = {
+		"product": {
+			"Key": key,
+			"Size": size,
+			"Color": color,
+			"Price": price,
+			"Type": type,
+			"Amount": amount
+		}
+	};
+
+	DoAjax("GET", "http://127.0.0.1/api/ViewProducts", fn, params); //0?
+}
