b6060501a44b3a36b76f47f6f9324e58ae31934e
Karolina <karolina.maison1@gmail.com>
2019-08-26 20:24:06 +0200

    Fix js

--- Client/bin/Debug/App_Data/scripts.js
+++ Client/bin/Debug/App_Data/scripts.js
@@ -291,23 +291,5 @@ function reserve_product(product) {
         }
     };
     //dodac tu time
-    return DoAjaxPOSTret("POST", "http://127.0.0.1/api/ReserveProduct",fn, params)
-}
-
-function DoAjaxPOSTret(method, url, fn, params) {
-    var request =  new XMLHttpRequest();
-    request.open(method, url, true);
-    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-
-    var handler = function (request) {
-        return function () {
-            if (request.readyState != 4) return;
-            if (request.status == 200) 
-                return fn(request);
-            else alert(request.readyState + " " + request.status + " " + request.statusText);
-        };
-    }
-    request.onreadystatechange = handler(request);
-    request.send(JSON.stringify(params));
-    console.log('res'+request.responseText);
+    return DoAjaxPOST("POST", "http://127.0.0.1/api/ReserveProduct",fn, params)
 }
